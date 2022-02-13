81553a6c9d8e111d18ddd604afe392f6b15eb157
Rodrigotex123 <72609610+Rodrigotex123@users.noreply.github.com>
2022-02-13 17:56:26 +0100

    Add files via upload

---
+++ aplicacion/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import requests
+from random import shuffle
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    donnees_obtenues=requests.get('https://api.dailymotion.com/videos?channel=news&limit=20')
+    donnes_JSON=donnees_obtenues.json()
+    videos = donnes_JSON['list'] 
+    shuffle(videos)
+    return render_template('index.html' , donees=videos[0:18])
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
